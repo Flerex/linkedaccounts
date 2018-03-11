ace987218ecb002593a53f63e0e7c2942daa6af0
Flerex <flerex@icloud.com>
2018-03-11 20:15:13 +0100

    English language corrections

--- language/en/common.php
+++ language/en/common.php
@@ -23,7 +23,7 @@ $lang = array_merge($lang, array(
 	
 	// UCP Management Module
 	'LINKED_ACCOUNTS_MANAGEMENT'			=> 'Account management',
-	'LINKED_ACCOUNTS_DESCRIPTION'			=> 'Here you will be able to link other accounts to the one you are currently logged in. Linking allows you to easily switch between different accounts without having to type your password every time.',
+	'LINKED_ACCOUNTS_DESCRIPTION'			=> 'Here you will be able to link other accounts to the one you are currently logged into. Linking allows you to easily switch between different accounts without having to type your password every time.',
 	'LINK_ACCOUNT'							=> 'Link account',
 	'ACCOUNT'								=> 'Account',
 	'LINKED_ON'								=> 'Linked on',
@@ -33,11 +33,11 @@ $lang = array_merge($lang, array(
 
 	// UCP Linking Module
 	'LINKING_ACCOUNT'						=> 'Account linking',
-	'ACCOUNT_LINKING_EXPLAIN'				=> 'Here you must introduce the credentials of the account you want to link to.',
+	'ACCOUNT_LINKING_EXPLAIN'				=> 'Here you must provide the credentials of the account you want to link to.',
 	'FIND_ACCOUNT'							=> 'Find account',
 	'CURRENT_PASSWORD_EXPLAIN_LINKACCOUNTS' => 'You must enter your current password if you wish to create a link to the previous account.',
 	'EMPTY_FIELDS'							=> 'Username and password cannot be empty.',
-	'INCORRECT_LINKED_ACCOUNT_CREDENTIALS'	=> 'The introduced credentials don\'t match any account.',
+	'INCORRECT_LINKED_ACCOUNT_CREDENTIALS'	=> 'The provided credentials don\'t match any account.',
 	'SAME_ACCOUNT'							=> 'You cannot link this account to itself!',
 	'INACTIVE_ACCOUNT'						=> 'The account you are trying to link to appears to be inactive.',
 	'BANNED_ACCOUNT'						=> 'The account you are trying to link to appears to be banned.',
