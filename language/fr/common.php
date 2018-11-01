5812092e398962528aae194ab335fb1779a312a6
Kidounet <kidou@kidou.net>
2018-11-01 23:12:39 +0100

    French translation
    Add translation for french language

---
+++ language/fr/common.php
@@ -0,0 +1,37 @@
+<?php
+/**
+*
+* Linked Accounts extension for phpBB 3.2
+*
+* @copyright (c) 2018 Flerex
+* @author Flerex <flerex@icloud.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+
+	// General translations
+	
+	// UCP Management Module
+
+	// UCP Linking Module
+
+	// Switching process
+
+	// ACP Overview Module
+
+	// ACP Management Module
+
+	// ACP Settings Module
+));
---
+++ language/fr/permissions_linkedaccounts.php
@@ -0,0 +1,24 @@
+<?php
+/**
+*
+* Linked Accounts extension for phpBB 3.2
+*
+* @copyright (c) 2018 Flerex
+* @author Flerex <flerex@icloud.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+
+));
