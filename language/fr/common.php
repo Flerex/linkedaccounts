5ee91cc415c614ca84b4ce04469158ce2a6a010c
Raphaël M <Galixte@users.noreply.github.com>
2019-06-20 16:38:53 +0200

    Fix the header in FRENCH language files.

--- language/fr/common.php
+++ language/fr/common.php
@@ -1,17 +1,14 @@
 <?php
 /**
  *
- * Linked Accounts. An extension for the phpBB Forum Software package.
- * French translation by Galixte (http://www.galixte.com)
+ * Linked Accounts extension for phpBB 3.2
  *
- * @copyright (c) 201-2019 Flerex <flerex@icloud.com>
- * @license GNU General Public License, version 2 (GPL-2.0-only)
+ * @copyright (c) 2018 Flerex
+ * @author        Flerex <flerex@icloud.com> | French translation by Galixte (http://www.galixte.com)
+ * @license       GNU General Public License, version 2 (GPL-2.0)
  *
  */
 
-/**
- * DO NOT CHANGE
- */
 if (!defined('IN_PHPBB'))
 {
 	exit;
@@ -22,23 +19,8 @@ if (empty($lang) || !is_array($lang))
 	$lang = array();
 }
 
-// DEVELOPERS PLEASE NOTE
-//
-// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
-//
-// Placeholders can now contain order information, e.g. instead of
-// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
-// translators to re-order the output of data while ensuring it remains correct
-//
-// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
-// equally where a string contains only two placeholders which are used to wrap text
-// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
-//
-// Some characters you may want to copy&paste:
-// ’ « » “ ” …
-//
-
 $lang = array_merge($lang, array(
+
 	// General translations
 	'LINKED_ACCOUNTS'                       => 'Comptes utilisateur liés',
 	'ADM_LINKED_ACCOUNTS'                   => 'Comptes utilisateur liés',
@@ -99,4 +81,5 @@ $lang = array_merge($lang, array(
 
 	// Posting as
 	'POSTING_AS'                            => 'Publié en tant que',
+
 ));
