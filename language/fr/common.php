0d2707089f3e88ac50bb6e506243cd7f38f1d950
Raphaël M <Galixte@users.noreply.github.com>
2019-06-20 17:36:50 +0200

    Update of the FRENCH translation for the Linked Accounts extension. (#19)
    * Update of the FRENCH translation for the Linked Accounts extension.
    
    * Update of the FRENCH translation for the Linked Accounts extension.
    
    * Fix the header in FRENCH language files.

--- language/fr/common.php
+++ language/fr/common.php
@@ -4,7 +4,7 @@
  * Linked Accounts extension for phpBB 3.2
  *
  * @copyright (c) 2018 Flerex
- * @author        Flerex <flerex@icloud.com>
+ * @author        Flerex <flerex@icloud.com> | French translation by Galixte (http://www.galixte.com)
  * @license       GNU General Public License, version 2 (GPL-2.0)
  *
  */
@@ -22,64 +22,64 @@ if (empty($lang) || !is_array($lang))
 $lang = array_merge($lang, array(
 
 	// General translations
-	'LINKED_ACCOUNTS'                       => 'Comptes Liés',
-	'ADM_LINKED_ACCOUNTS'                   => 'Comptes Liés',
+	'LINKED_ACCOUNTS'                       => 'Comptes utilisateur liés',
+	'ADM_LINKED_ACCOUNTS'                   => 'Comptes utilisateur liés',
 
 	// UCP Management Module
-	'LINKED_ACCOUNTS_MANAGEMENT'            => 'Gestion du compte',
-	'LINKED_ACCOUNTS_DESCRIPTION'           => 'Ici vous pourrez associer d’autres comptes à celui auquel vous êtes actuellement connecté. La liaison vous permet de basculer facilement entre différents comptes sans avoir à saisir votre mot de passe à chaque fois.',
-	'LINK_ACCOUNT'                          => 'Lier les comptes',
-	'ACCOUNT'                               => 'Compte',
-	'LINKED_ON'                             => 'Liaison',
-	'NO_LINKED_ACCOUNTS'                    => 'Il n’y a pas de comptes liés.',
-	'UNLINK_ACCOUNT'                        => 'Dissocier le compte',
-	'SUCCESSFUL_UNLINKING'                  => 'Comptes dissociés avec succès.',
+	'LINKED_ACCOUNTS_MANAGEMENT'            => 'Gestion des comptes utilisateurs',
+	'LINKED_ACCOUNTS_DESCRIPTION'           => 'Permet d’associer d’autres comptes utilisateur à celui actuellement utilisé. En associant des comptes utilisateur il devient alors possible de permuter facilement entre eux sans avoir besoin de fournir ses identifiants de connexion.',
+	'LINK_ACCOUNT'                          => 'Associer un compte utilisateur',
+	'ACCOUNT'                               => 'Noms d’utilisateur des comptes associés',
+	'LINKED_ON'                             => 'Associé le',
+	'NO_LINKED_ACCOUNTS'                    => 'Il n’y a pas de comptes associés.',
+	'UNLINK_ACCOUNT'                        => 'Dissocier des comptes utilisateur',
+	'SUCCESSFUL_UNLINKING'                  => 'Comptes utilisateurs dissociés avec succès.',
 
 	// UCP Linking Module
-	'LINKING_ACCOUNT'                       => 'Lien de compte',
-	'ACCOUNT_LINKING_EXPLAIN'               => 'Ici vous devez fournir les informations d’identification du compte auquel vous souhaitez vous connecter.',
-	'FIND_ACCOUNT'                          => 'Trouver un compte',
-	'CURRENT_PASSWORD_EXPLAIN_LINKACCOUNTS' => 'Vous devez entrer votre mot de passe actuel si vous souhaitez créer un lien vers le compte précédent.',
-	'EMPTY_FIELDS'                          => 'Le nom d’utilisateur et le mot de passe ne peuvent pas être vides.',
+	'LINKING_ACCOUNT'                       => 'Association de comptes utilisateurs',
+	'ACCOUNT_LINKING_EXPLAIN'               => 'Permet de saisir les informations d’identification du compte utilisateur à associer.',
+	'FIND_ACCOUNT'                          => 'Rechercher un membre',
+	'CURRENT_PASSWORD_EXPLAIN_LINKACCOUNTS' => 'Il est nécessaire de saisir le mot de passe du compte utilisateur actuellement utilisé pour générer une association avec le compte renseigné ci-dessus.',
+	'EMPTY_FIELDS'                          => 'Un nom d’utilisateur et son mot de passe correspondant doivent être renseigné.',
 	'INCORRECT_LINKED_ACCOUNT_CREDENTIALS'  => 'Les informations d’identification fournies ne correspondent à aucun compte.',
-	'SAME_ACCOUNT'                          => 'Vous ne pouvez pas lier ce compte à lui-même !',
+	'SAME_ACCOUNT'                          => 'Il n’est pas possible d’associer ce compte à lui-même !',
 	'INACTIVE_ACCOUNT'                      => 'Le compte auquel vous essayez de vous connecter semble être inactif.',
 	'BANNED_ACCOUNT'                        => 'Le compte auquel vous essayez de vous connecter semble être banni.',
-	'ALREADY_LINKED'                        => 'Vous êtes déja lié à ce compte.',
+	'ALREADY_LINKED'                        => 'Vous êtes déjà lié à ce compte.',
 
 	// Switching process
-	'ACCOUNTS_SWITCHED'                     => 'Comptes échangés avec succès.',
-	'INVALID_LINKED_ACCOUNT'                => 'Vous ne pouvez pas basculer sur ce compte.',
+	'ACCOUNTS_SWITCHED'                     => 'Comptes permutés avec succès !',
+	'INVALID_LINKED_ACCOUNT'                => 'Il n’est pas possible de permuter vers ce compte.',
 
 	// ACP Overview Module
 	'ADM_LINKED_ACCOUNTS_OVERVIEW'          => 'Vue d’ensemble',
-	'ADM_LINKED_ACCOUNTS_OVERVIEW_EXPLAIN'  => 'Dans cette section, vous trouverez des statistiques d’utilisation ainsi qu’une liste des utilisateurs ayant des liens vers d’autres comptes.',
-	'LINKED_ACCOUNTS_COUNT'                 => 'Utilisateurs avec des liens',
-	'LINKED_ACCOUNTS_COUNT_EXPLAIN'         => 'Nombre de comptes ayant au moins un lien.',
-	'LINK_COUNT'                            => 'Liens',
-	'LINK_COUNT_EXPLAIN'                    => 'Quantité totale de liens créés.',
-	'NO_ACCOUNTS_LINKED'                    => 'Il n’y a pas de compte avec des liens.',
+	'ADM_LINKED_ACCOUNTS_OVERVIEW_EXPLAIN'  => 'Permet de consulter les statistiques d’utilisation ainsi que la liste des membres ayant des associations de comptes utilisateur.',
+	'LINKED_ACCOUNTS_COUNT'                 => 'Membres ayant des associations',
+	'LINKED_ACCOUNTS_COUNT_EXPLAIN'         => 'Nombre de comptes ayant au moins une association.',
+	'LINK_COUNT'                            => 'Associations',
+	'LINK_COUNT_EXPLAIN'                    => 'Total des associations effectuées.',
+	'NO_ACCOUNTS_LINKED'                    => 'Il n’y a aucun compte associé.',
 
 	// ACP Management Module
-	'ADM_LINKED_ACCOUNTS_MANAGEMENT'        => 'Gérer les utilisateurs',
-	'SELECT_USER'                           => 'Sélectionner un utilisateur',
-	'MANAGING_USER'                         => 'Gestion de l’utilisateur :: %s',
-	'ACCOUNT_LINKS'                         => 'Comptes liés',
-	'LINK_ACCOUNTS'                         => 'Lier les comptes',
-	'LINK_ACCOUNTS_EXPLAIN'                 => 'Ici, vous pouvez créer des liens pour cet utilisateur.',
-	'SUCCESSFUL_MULTI_LINK_CREATION'        => 'Liens créés avec succès.',
+	'ADM_LINKED_ACCOUNTS_MANAGEMENT'        => 'Gérer les membres',
+	'SELECT_USER'                           => 'Sélectionner un membre',
+	'MANAGING_USER'                         => 'Gestion du membre :: %s',
+	'ACCOUNT_LINKS'                         => 'Comptes associés',
+	'LINK_ACCOUNTS'                         => 'Associer des comptes',
+	'LINK_ACCOUNTS_EXPLAIN'                 => 'Permet de créer des associations pour ce membre.',
+	'SUCCESSFUL_MULTI_LINK_CREATION'        => 'Associations effectuées avec succès !',
 
 	// ACP Settings Module
-	'ADM_LINKED_ACCOUNTS_SETTINGS'          => 'Réglages',
-	'ADM_LINKED_ACCOUNTS_SETTINGS_EXPLAIN'  => 'Ici, vous pouvez personnaliser certaines fonctionnalités de l’extension.',
-	'CONF_AJAX'                             => 'Utilisez AJAX pour changer de compte',
-	'CONF_AJAX_EXPLAIN'                     => 'Activer cette option vous redirigera automatiquement, sans avoir à passer par la page «Information». Les utilisateurs sans support AJAX vont quand même parcourir cette page.',
-	'CONF_RETURN_TO_INDEX'                  => 'Retour à l’index lors du changement de compte',
-	'CONF_RETURN_TO_INDEX_EXPLAIN'          => 'Si non activé, le changement de compte retournera à la même page par défaut.',
-	'CONF_PRIVATE_LINKS'                    => 'Liens privés',
-	'CONF_PRIVATE_LINKS_EXPLAIN'            => 'Si vous définissez cette option sur «oui», le menu de basculement sera masqué lorsqu’un utilisateur ne dispose pas d’autorisations de basculement, même lorsque le compte dispose de liens. Cela pourrait constituer un risque pour la sécurité et il est recommandé de le laisser désactivé.',
+	'ADM_LINKED_ACCOUNTS_SETTINGS'          => 'Paramètres',
+	'ADM_LINKED_ACCOUNTS_SETTINGS_EXPLAIN'  => 'Permet de personnaliser certaines fonctionnalités de l’extension.',
+	'CONF_AJAX'                             => 'Utiliser le langage AJAX pour permuter entre les comptes utilisateur',
+	'CONF_AJAX_EXPLAIN'                     => 'Permet d’activer cette option qui permettera automatiquement le membre vers un autre compte utilisateur sans avoir à s’identifier. Les utilisateurs n’ayant pas activé le support du langage AJAX seront eux aussi redirigés vers cette page.',
+	'CONF_RETURN_TO_INDEX'                  => 'Retourner vers la page de l’index du forum lors de la permutation entre comptes utilisateurs',
+	'CONF_RETURN_TO_INDEX_EXPLAIN'          => 'Permet d’activer la redirection vers la page de l’index du forum après avoir permuté de compte utilisateur. Dans le cas contraire, si cette option est désactivée la redirection s’effectuera vers la même page qu’avant la permutation.',
+	'CONF_PRIVATE_LINKS'                    => 'Associations privés',
+	'CONF_PRIVATE_LINKS_EXPLAIN'            => 'Permet de masquer l’option de permutation de comptes utilisateur pour les membres n’ayant pas l’autorisation de permuter et alors même que le membre aurait une association établie entre deux compte utilisateur. Il se peut que cela occasionne un risque pour la sécurité aussi il est recommandé de laisser cette option désactivée.',
 
 	// Posting as
-	'POSTING_AS'                            => 'Posting as',
+	'POSTING_AS'                            => 'Publié en tant que',
 
 ));
--- language/fr/permissions_linkedaccounts.php
+++ language/fr/permissions_linkedaccounts.php
@@ -4,7 +4,7 @@
  * Linked Accounts extension for phpBB 3.2
  *
  * @copyright (c) 2018 Flerex
- * @author        Flerex <flerex@icloud.com>
+ * @author        Flerex <flerex@icloud.com> | French translation by Galixte (http://www.galixte.com)
  * @license       GNU General Public License, version 2 (GPL-2.0)
  *
  */
@@ -21,10 +21,10 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 
-	'ACL_U_SWITCH_ACCOUNTS'                  => 'Peut changer de compte',
-	'ACL_U_LINK_ACCOUNTS'                    => 'Peut lier des comptes',
-	'ACL_A_LINK_ACCOUNTS'                    => 'Peut gérer les liens de compte',
-	'ACL_U_POST_AS_ACCOUNT'                  => 'Can post as a one of their linked accounts',
-	'ACL_U_VIEW_OTHER_USERS_LINKED_ACCOUNTS' => 'View other user’s linked accounts',
+	'ACL_U_SWITCH_ACCOUNTS'                  => 'Peut permuter de compte utilisateur.',
+	'ACL_U_LINK_ACCOUNTS'                    => 'Peut associer des comptes utilisateur.',
+	'ACL_A_LINK_ACCOUNTS'                    => 'Peut gérer les comptes utilisateur associés.',
+	'ACL_U_POST_AS_ACCOUNT'                  => 'Peut publier des messages avec un de ses comptes utilisateur associés.',
+	'ACL_U_VIEW_OTHER_USERS_LINKED_ACCOUNTS' => 'Peut voir les comptes utilisateur associés des autres membres.',
 
 ));
