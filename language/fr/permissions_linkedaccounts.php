d23fafafd79a1cd4106522772c663ee7973b3bf2
Raphaël M <Galixte@users.noreply.github.com>
2019-06-20 04:53:18 +0200

    Update of the FRENCH translation for the Linked Accounts extension.

--- language/fr/permissions_linkedaccounts.php
+++ language/fr/permissions_linkedaccounts.php
@@ -1,14 +1,17 @@
 <?php
 /**
  *
- * Linked Accounts extension for phpBB 3.2
+ * Linked Accounts. An extension for the phpBB Forum Software package.
+ * French translation by Galixte (http://www.galixte.com)
  *
- * @copyright (c) 2018 Flerex
- * @author        Flerex <flerex@icloud.com>
- * @license       GNU General Public License, version 2 (GPL-2.0)
+ * @copyright (c) 201-2019 Flerex <flerex@icloud.com>
+ * @license GNU General Public License, version 2 (GPL-2.0-only)
  *
  */
 
+/**
+ * DO NOT CHANGE
+ */
 if (!defined('IN_PHPBB'))
 {
 	exit;
@@ -19,12 +22,26 @@ if (empty($lang) || !is_array($lang))
 	$lang = array();
 }
 
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ « » “ ” …
+//
+
 $lang = array_merge($lang, array(
-
-	'ACL_U_SWITCH_ACCOUNTS'                  => 'Peut changer de compte',
-	'ACL_U_LINK_ACCOUNTS'                    => 'Peut lier des comptes',
-	'ACL_A_LINK_ACCOUNTS'                    => 'Peut gérer les liens de compte',
-	'ACL_U_POST_AS_ACCOUNT'                  => 'Can post as a one of their linked accounts',
-	'ACL_U_VIEW_OTHER_USERS_LINKED_ACCOUNTS' => 'View other user’s linked accounts',
-
+	'ACL_U_SWITCH_ACCOUNTS'                  => 'Peut permuter de compte utilisateur.',
+	'ACL_U_LINK_ACCOUNTS'                    => 'Peut associer des comptes utilisateur.',
+	'ACL_A_LINK_ACCOUNTS'                    => 'Peut gérer les comptes utilisateur associés.',
+	'ACL_U_POST_AS_ACCOUNT'                  => 'Peut publier des messages avec un de ses comptes utilisateur associés.',
+	'ACL_U_VIEW_OTHER_USERS_LINKED_ACCOUNTS' => 'Peut voir les comptes utilisateur associés des autres membres.',
 ));
